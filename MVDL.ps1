1bfd2c3e8639e9dfe632019c444d22d0d803f797
Zaph-x <mikkel@luceartfoto.dk>
2020-01-14 00:36:19 +0100

    Initial commit

---
+++ MVDL.ps1
@@ -0,0 +1,94 @@
+Import-Module -Name Selenium
+
+$time = Get-Date -Format "ddMMyyyy-HHmmss"
+$location = Get-Location
+
+$moodleBaseUrl = "https://www.moodle.aau.dk"
+$moodleLoginUrl = "https://www.moodle.aau.dk/login"
+$moodleCourseUrl = "https://www.moodle.aau.dk/course/view.php?id=31429"
+$transcriptFile = ".\transcript - $($time).txt"
+
+$username = Read-Host -Prompt "Enter your username: "
+$password = Read-Host -Prompt "Enter your password: "
+$i = 1
+
+
+function DownloadVideosOnPage {
+    param (
+        $url
+    )
+    $videos = @()
+    Find-SeElement -TagName a -Driver $Driver | Where-Object {$_.GetAttribute("href") -like "$moodleBaseUrl*.mp4"} | ForEach-Object {
+        $videos += $_.GetAttribute("href")
+    }
+
+    foreach ($video in $videos) {
+        DownloadFromUrl $video $url
+    }
+    
+}
+
+
+function DownloadFromUrl {
+    param (
+        $url,
+        $returnAddress
+    )
+    Enter-SeUrl -Driver $Driver -Url $url
+    [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+    [System.Windows.Forms.SendKeys]::SendWait("^{s}")
+    Start-Sleep -Seconds 4
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+    Start-Sleep -Seconds 2
+
+}
+
+
+function DownloadFromPage {
+    param (
+        $url
+    )
+    Enter-SeUrl -Driver $Driver -Url $url
+    $pageAnchors = Find-SeElement -Driver $Driver -TagName a | Where-Object {$_.GetAttribute("href") -match "$($moodleBaseUrl)/mod/page/view\.php\?id=\d+$"}
+    $anchors = @()
+
+    foreach ($anchor in $pageAnchors) {
+        $anchors += $anchor.GetAttribute("href")
+    }
+
+    foreach ($link in $anchors) {
+        if ($link -notlike $url) {
+            DownloadFromPage $link
+        }
+    }
+    DownloadVideosOnPage $url
+}
+
+
+
+$Driver = Start-SeChrome -DisableBuiltInPDFViewer $true -HideVersionHint -DefaultDownloadPath $location.Path
+$window = Get-SeWindow -Driver $Driver
+
+if ($Driver) {
+    
+    Enter-SeUrl -Driver $Driver -Url $moodleLoginUrl
+    
+    $emailField = Find-SeElement -Id "username" -Driver $Driver 
+   
+    Send-SeKeys -Element $emailField -Keys "$($username)@student.aau.dk"
+    $pwField = Find-SeElement -Id "password" -Driver $Driver
+    Send-SeKeys -Element $pwField -Keys $password
+    Send-SeKeys -Element $pwField -Keys ([OpenQA.Selenium.Keys]::Enter)
+
+    Enter-SeUrl -Driver $Driver -Url $moodleCourseUrl
+    
+    DownloadVideosOnPage $moodleCourseUrl
+    DownloadFromPage $moodleCourseUrl
+    
+  
+
+    
+
+
+}
+    
---
+++ README.md
@@ -0,0 +1,12 @@
+# MVDL (Moodle Video Downloader)
+
+This script will download vidoes from moodle recursively, by using Selenium to traverse the page.
+
+The script uses Selenium to traverse the page, meaning you must have the Selenium module installed.
+
+This can be done, by running the following command in a Powershell terminal:
+
+```powershell
+PS> Install-Module Selenium
+```
+
